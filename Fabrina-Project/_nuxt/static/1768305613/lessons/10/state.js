3931d638ac149e9b171fb33126f847680465b52d
Ali-Romero <sherali920825@yandex.ru>
2026-01-13 18:00:18 +0600

    Updates

---
+++ Fabrina-Project/_nuxt/static/1768305613/lessons/10/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002FFabrina-Project\u002FFabrina-Project\u002F_nuxt\u002Fstatic\u002F1768305613",layout:"default",error:a,serverRendered:true,routePath:"\u002Flessons\u002F10",config:{headerHeight:66,_app:{basePath:"\u002FFabrina-Project\u002F",assetsPath:"\u002FFabrina-Project\u002FFabrina-Project\u002F_nuxt\u002F",cdnURL:a}}}}(null));
--- _nuxt/static/1768305259/lessons/8/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002FFabrina-Project\u002F_nuxt\u002Fstatic\u002F1768305259",layout:"default",error:a,serverRendered:true,routePath:"\u002Flessons\u002F8",config:{headerHeight:66,_app:{basePath:"\u002FFabrina-Project\u002F",assetsPath:"\u002FFabrina-Project\u002F_nuxt\u002F",cdnURL:a}}}}(null));
