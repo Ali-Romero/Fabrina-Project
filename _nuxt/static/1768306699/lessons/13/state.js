494a7b5329176bca6120f7fd6dde238f0d5f8270
Ali-Romero <sherali920825@yandex.ru>
2026-01-13 18:18:24 +0600

    Updates

---
+++ _nuxt/static/1768306699/lessons/13/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002FFabrina-Project\u002F_nuxt\u002Fstatic\u002F1768306699",layout:"default",error:a,serverRendered:true,routePath:"\u002Flessons\u002F13",config:{headerHeight:66,_app:{basePath:"\u002FFabrina-Project\u002F",assetsPath:"\u002FFabrina-Project\u002F_nuxt\u002F",cdnURL:a}}}}(null));
